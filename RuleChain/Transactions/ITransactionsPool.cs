a80668f44a515051bfb62f14f0f68b7f336eb33f
Aleksey <fefaleksey@gmail.com>
2019-04-03 22:26:08 +0300

    add stupid transaction pool

--- RuleChain/Transactions/ITransactionsPool.cs
+++ RuleChain/Transactions/ITransactionsPool.cs
@@ -1,7 +1,22 @@
-﻿namespace RuleChain.Transactions
+﻿using System.Collections.Generic;
+
+namespace RuleChain.Transactions
 {
     internal interface ITransactionsPool
     {
+        void Push(ITransaction transaction);
         
+        /// <summary>
+        /// Get 1 transaction from pool.
+        /// </summary>
+        /// <returns>Transaction.</returns>
+        ITransaction Get();
+        
+        /// <summary>
+        /// Get list of transactions from pool.
+        /// </summary>
+        /// <param name="quantity">Number of transactions.</param>
+        /// <returns>List of "quantity" (or less if there are not enough) transaction.</returns>
+        List<ITransaction> Get(int quantity);
     }
 }
