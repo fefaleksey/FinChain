e066490db64f67ff8aefea026f875225ff64a864
Aleksey <fefaleksey@gmail.com>
2019-04-25 01:32:43 +0300

    fix rule transaction

--- RuleChain.Models/RuleTransaction.cs
+++ RuleChain.Models/RuleTransaction.cs
@@ -9,25 +9,50 @@ namespace RuleChain.Models
         public DateTime Time { get; }
         public TransactionStatus Status { get; set; }
         public TransactionType Type { get; }
-        public ActionId ActionId { get; }
-        public RequirementId RequirementId { get; }
-        public IAction Action { get; }
+        public ActionType ActionTypeKey { get; }
+        public ActionType ActionTypeValue { get; }
         public IActionRequirements Requirements { get; }
         public int Step { get; }
         public int Position { get; }
 
-        public RuleTransaction(TransactionType type, ActionId actionId, RequirementId requirementId, IAction action, 
-            IActionRequirements requirements, int step, int position)
+        private RuleTransaction(TransactionType type, ActionType actionTypeKey, ActionType actionTypeValue
+            , IActionRequirements requirements, int step, int position)
         {
             Type = type;
-            ActionId = actionId;
-            RequirementId = requirementId;
-            Action = action;
+            ActionTypeKey = actionTypeKey;
+            ActionTypeValue = actionTypeValue;
             Requirements = requirements;
             Step = step;
             Position = position;
             Status = TransactionStatus.Created;
             Time = DateTime.UtcNow;
+        }
+//        TODO: Add following:
+//        RemoveRequirements,
+//        AddActionToRequirement,
+//        RemoveActionFromRequirement,
+
+        public static RuleTransaction CreateAddActionTransaction(ActionType actionTypeKey,
+            IActionRequirements requirements)
+        {
+            var transaction = new RuleTransaction(TransactionType.AddRequirements, actionTypeKey, ActionType.Null
+                , requirements, 0, 0);
+            return transaction;
+        }
+
+        public static RuleTransaction CreateRemoveActionTransaction(ActionType actionTypeKey)
+        {
+            var transaction = new RuleTransaction(TransactionType.AddRequirements, actionTypeKey, ActionType.Null
+                , null, 0, 0);
+            return transaction;
+        }
+
+        public static RuleTransaction CreateAddRequirementsTransaction(ActionType actionTypeKey,
+            ActionType actionTypeValue, IActionRequirements requirements, int step)
+        {
+            var transaction = new RuleTransaction(TransactionType.AddRequirements, actionTypeKey,
+                actionTypeValue, requirements, step, 0);
+            return transaction;
         }
     }
 }
