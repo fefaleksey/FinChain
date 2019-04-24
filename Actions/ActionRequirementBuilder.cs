7de7d31526ac79f513e7429cf830d7f126db9bc7
Aleksey <fefaleksey@gmail.com>
2019-04-25 01:20:36 +0300

    fix requirements logic

---
+++ Actions/ActionRequirementBuilder.cs
@@ -0,0 +1,37 @@
+using System;
+using FinChain.Models.Actions;
+
+namespace Actions
+{
+    public static class ActionRequirementBuilder
+    {
+        public static IActionRequirements Create(ActionType type)
+        {
+            switch (type)
+            {
+                case ActionType.TransferFromPersonToPerson:
+                {
+                    return CreateTransferFromPersonToPersonActionRequirements();
+                }
+                case ActionType.PayTax:
+                {
+                    return CreatePayTaxActionRequirements();
+                }
+                default:
+                {
+                    throw new NotSupportedException();
+                }
+            }
+        }
+
+        private static IActionRequirements CreateTransferFromPersonToPersonActionRequirements()
+        {
+            return new TransferFromPersonToPersonActionRequirements();
+        }
+
+        private static IActionRequirements CreatePayTaxActionRequirements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
