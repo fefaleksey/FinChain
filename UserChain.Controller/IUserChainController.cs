82a9896c49871429a9edc88280f3c326cd5d3330
Aleksey <fefaleksey@gmail.com>
2019-05-01 22:54:43 +0300

    add simple implementation of user chain controller

--- UserChain.Controller/IUserChainController.cs
+++ UserChain.Controller/IUserChainController.cs
@@ -1,6 +1,11 @@
+using UserChain.Transactions;
+
 namespace UserChain.Controller
 {
     public interface IUserChainController
-    {        
+    {
+        void CommitBlock(Block block);
+
+        int GetLastBlockHash();
     }
 }
--- UserChain.Controller/UserChainController.cs
+++ UserChain.Controller/UserChainController.cs
@@ -1,6 +1,21 @@
-﻿namespace UserChain.Controller
+﻿using UserChain.Transactions;
+
+namespace UserChain.Controller
 {
     public class UserChainController : IUserChainController
     {
+        private readonly UserChain _chain;
+        public UserChainController(UserChain chain)
+        {
+            _chain = chain;
+        }
+
+        public void CommitBlock(Block block) => _chain.CommitBlock(block);
+
+        public int GetLastBlockHash()
+        {
+            var block = _chain.GetLastBlock();
+            return block.Hash;
+        }
     }
 }
