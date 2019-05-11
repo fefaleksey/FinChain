b71779aff53fb949150399a96dd9e5006d14a6aa
Aleksey <fefaleksey@gmail.com>
2019-05-12 01:35:51 +0300

    refactor state

--- UserChain/State.cs
+++ UserChain/State.cs
@@ -2,21 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using FinChain.Models.Accounts;
-using FinChain.Models.Actions;
 using UserChain.Models;
 using UserChain.Models.Enums;
-using Microsoft.CSharp;
-using System.CodeDom.Compiler;
 using System.IO;
 using System.Linq;
 using System.Runtime.Loader;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Emit;
-
-//using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
 
 namespace UserChain
 {
@@ -51,111 +44,7 @@ namespace UserChain
 
         private void Deploy_Handler(UserChainTransaction transaction)
         {
-//            transaction.
-            var sender = transaction.Sender;
-            var action = (IAction) transaction.Params[0];
-            foreach (var accountType in action.AccessToDeploy)
-            {
-                if (sender.Type == accountType)
-                {
-//                    Deploy(action);
-                }
-            }
-        }
-
-//        [HttpPost, Route("kekPost")]
-//        public void Kek([FromBody] string code)
-//        {
-//            System.IO.File.WriteAllText("source.cs", code);
-////          
-//            Microsoft.CSharp.RuntimeBinder.Binder.CSharpCodeProvider codeProvider = new CSharpCodeProvider();
-//            ICodeCompiler icc = codeProvider.CreateCompiler();
-//            System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
-//            parameters.GenerateExecutable = false;
-//            parameters.OutputAssembly = "AutoGen.dll";
-//            CompilerResults results = icc.CompileAssemblyFromSource(parameters, yourCodeAsString);
-//
-//            var assembly = Assembly.Load("assemblyName");
-//            var type = assembly.GetType("...Contract");
-//            var obj = Activator.CreateInstance(type) as dynamic;
-//            obj.Execute();
-//            
-//            object[] @params = new object[2];
-//            @params[0] = 'a';
-//            @params[1] = 'b';
-//
-// /* 2. invoke method MyMethod() which returns object "CustomType" - how do I check if it exists? */
-// /* 3. what's the meaning of 4th parameter (t in this case); MSDN says this is "the Object on which to
-//       invoke the specified member", but isn't this already accounted for by using t.InvokeMember()? */
-
-//            var result = type.InvokeMember("MyMethod", BindingFlags.InvokeMethod, null, type, @params);
-//            var shit = result;
-//
-//        }
-
-        //TODO: private
-
-        public void Deploy(string code)
-        {
-            var provider = new CSharpCodeProvider();
-            var parameters = new CompilerParameters();
-
-
-            // Reference to System.Drawing library
-            parameters.ReferencedAssemblies.Add("System.Drawing.dll");
-            // True - memory generation, false - external file generation
-            parameters.GenerateInMemory = true;
-            // True - exe file generation, false - dll file generation
-            parameters.GenerateExecutable = true;
-
-            CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
-
-
-            if (results.Errors.HasErrors)
-            {
-                StringBuilder sb = new StringBuilder();
-
-                foreach (CompilerError error in results.Errors)
-                {
-                    sb.AppendLine(String.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
-                }
-
-                throw new InvalidOperationException(sb.ToString());
-            }
-
-            Assembly assembly = results.CompiledAssembly;
-
-
-            Type program = assembly.GetType("First.Contract");
-            var obj = Activator.CreateInstance(program);
-            MethodInfo main = program.GetMethod("Main");
-
-            main.Invoke(null, null);
-            main.Invoke(obj, null);
-            /*
-            var assembly = Assembly.Load("assemblyName");
-            var type = assembly.GetType("...Contract");
-            var obj = Activator.CreateInstance(type) as dynamic;
-            obj.Execute();
-            */
-
-//            _actions.Add(action.Id, action);
-        }
-
-        public void Blyat(string code)
-        {
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
-//            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(@"
-//            namespace ns{
-//                using System;
-//                public class App{
-//                    public static void Main(string[] args){
-//                        Console.Write(""dada"");
-//                    }
-//                }
-//
-//
-//            }");
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(transaction.Code);
 
             //creating options that tell the compiler to output a .Net module
             var options = new CSharpCompilationOptions(
@@ -216,13 +105,9 @@ namespace UserChain
                     
                     var type = assembly.GetType("First.Contract");
                     var obj = Activator.CreateInstance(type) as dynamic;
-                    obj.Execute();
-                    obj.Execute();
-                    obj.Execute();
-                    
-                    
-//                    assembly.EntryPoint.Invoke(null, new object[] {new string[] {"arg1", "arg2", "etc"}});
+//                    obj.Execute();
 
+                    _contracts.Add(transaction.ContractId, obj);
                 }
             }
         }
@@ -230,7 +115,8 @@ namespace UserChain
         private void CallContractFunction_Handler(UserChainTransaction transaction)
         {
             var id = transaction.ContractId;
-            _actions[id].Execute(transaction.Sender, transaction.Params);
+            var contract = _contracts[id] as dynamic;
+            contract.Execute(transaction.Sender, transaction.Params);
         }
     }
 }
