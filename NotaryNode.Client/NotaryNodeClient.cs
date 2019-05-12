e10e341323cb6b5749cc10e12e9c48bd684382da
Aleksey <fefaleksey@gmail.com>
2019-05-12 03:19:59 +0300

    fix client

--- NotaryNode.Client/NotaryNodeClient.cs
+++ NotaryNode.Client/NotaryNodeClient.cs
@@ -2,10 +2,10 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using FinChain.Models;
 using FinChain.Models.Actions;
 using Newtonsoft.Json;
 using RuleChain.Models;
+using UserChain.Models;
 
 namespace NotaryNode.Client
 {
@@ -18,46 +18,48 @@ namespace NotaryNode.Client
             _httpClient = httpClient;
         }
 
-        public void AddTransactionEvent(string nodeUrl, TransactionEvent transactionEvent)
-        {
-//            Newtonsoft.Json.JsonConvert.SerializeObject
-            _httpClient.PostAsync($"{nodeUrl}/api/transactions/addEvent", null);
-        }
-
-        public void AddTransactionToPool(string nodeUrl, RuleTransaction ruleTransaction)
-        {
-            var json = JsonConvert.SerializeObject(ruleTransaction);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.PostAsync($"{nodeUrl}/api/transactions/addToPool", content);
-        }
-
         public async void SendBlock(string nodeUrl, RuleBlock block)
         {
             var settings = new JsonSerializerSettings()
             {
                 TypeNameHandling = TypeNameHandling.All
             };
-//            const Formatting indented = Formatting.Indented;
             var json = JsonConvert.SerializeObject(block, settings);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            // plug for debugging
             try
             {
-                await _httpClient.PostAsync($"{nodeUrl}/api/transactions/addBlock", content);
+                await _httpClient.PostAsync($"{nodeUrl}/api/RuleChainApiGateway/addBlock", content);
             }
             catch (Exception e)
             {
-                Console.WriteLine("===========================================================================");
                 Console.WriteLine(e);
-                Console.WriteLine("===========================================================================");
+            }
+        }
+
+        public async void SendBlock(string nodeUrl, UserChainBlock block)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+            var json = JsonConvert.SerializeObject(block, settings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                await _httpClient.PostAsync($"{nodeUrl}/api/UserChainApiGateway/addBlock", content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
 
         // TODO: implement
         public async Task<IActionRequirements> GetRequirements(string nodeUrl, ActionType action)
         {
-            var lol = await _httpClient.GetAsync($"{nodeUrl}/api/transactions/getRequirements/?action={action}");
+            var lol = await _httpClient.GetAsync($"{nodeUrl}/api/RuleChainApiGateway/getRequirements/?action={action}");
             var kek = await lol.Content.ReadAsStringAsync();
             var requirements = JsonConvert.DeserializeObject<ActionRequirements>(kek);
             return requirements;
