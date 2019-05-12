5c57bbc075babcec432b952a4d1cb63c43852e0a
Aleksey <fefaleksey@gmail.com>
2019-05-12 03:19:09 +0300

    add ordering service

---
+++ OrderingService/Controllers/TransactionsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using UserChain.Models;
+
+namespace OrderingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        
+        // POST api/values
+        [HttpPost, Route("add")]
+        public void AddTransaction([FromBody] UserChainTransaction transaction)
+        {
+            
+        }
+    }
+}
