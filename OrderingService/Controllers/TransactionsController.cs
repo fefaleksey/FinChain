8b527c165ef8e214514e033ca76fba7df0ecd2e6
Aleksey <fefaleksey@gmail.com>
2019-05-12 03:47:07 +0300

    add functionality

--- OrderingService/Controllers/TransactionsController.cs
+++ OrderingService/Controllers/TransactionsController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using TransactionPool;
+using UserChain;
 using UserChain.Models;
 
 namespace OrderingService.Controllers
@@ -8,6 +10,12 @@ namespace OrderingService.Controllers
     [ApiController]
     public class TransactionsController : ControllerBase
     {
+        private readonly ITransactionsPool<UserChainTransaction> _transactionsPool;
+        public TransactionsController(ITransactionsPool<UserChainTransaction> transactionsPool)
+        {
+            _transactionsPool = transactionsPool;
+        }
+        
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -38,7 +46,8 @@ namespace OrderingService.Controllers
         [HttpPost, Route("add")]
         public void AddTransaction([FromBody] UserChainTransaction transaction)
         {
-            
+            UserChainVerifier.Verify(transaction);
+            _transactionsPool.Push(transaction);
         }
     }
 }
