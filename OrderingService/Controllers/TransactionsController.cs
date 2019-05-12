20f7954a1a3319d78751e5106a54ef064a8f6953
Aleksey <fefaleksey@gmail.com>
2019-05-12 03:47:59 +0300

    delete templates methods

--- OrderingService/Controllers/TransactionsController.cs
+++ OrderingService/Controllers/TransactionsController.cs
@@ -16,32 +16,6 @@ namespace OrderingService.Controllers
             _transactionsPool = transactionsPool;
         }
         
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] {"value1", "value2"};
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-        
         // POST api/values
         [HttpPost, Route("add")]
         public void AddTransaction([FromBody] UserChainTransaction transaction)
