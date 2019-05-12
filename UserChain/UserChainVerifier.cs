a0d4d426e06400bd5aefa4b7ad49f0b7c4b613ba
Aleksey <fefaleksey@gmail.com>
2019-05-12 03:19:25 +0300

    add verifier plug

---
+++ UserChain/UserChainVerifier.cs
@@ -0,0 +1,17 @@
+using UserChain.Models;
+
+namespace UserChain
+{
+    public static class UserChainVerifier
+    {
+        public static void Verify(UserChainBlock block)
+        {
+            
+        }
+        
+        public static bool CheckCorrectness(UserChainBlock block)
+        {
+            return true;
+        }
+    }
+}
